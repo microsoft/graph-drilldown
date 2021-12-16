71167251a71174ab00e2bf932b8cc5c55a6304cb
Dayenne Souza <ddesouza@microsoft.com>
2021-12-16 16:13:16 -0300

    test lint fix

--- src/state/vis/view.ts
+++ src/state/vis/view.ts
@@ -2,9 +2,9 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
+import { atom, selector, useRecoilValue, useSetRecoilState } from 'recoil'
 import { ViewType } from '../../types'
 import { featuresState } from '../features'
-import { atom, selector, useRecoilValue, useSetRecoilState } from 'recoil'
 
 export const graphViewState = atom<ViewType>({
 	key: 'graph-view',
