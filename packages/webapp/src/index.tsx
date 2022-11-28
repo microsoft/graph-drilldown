7189ebf46913d93cd612ebe8497873cbf5d2ac18
natoverse <github@talkswithnumbers.com>
2022-11-28 13:17:32 -0800

    Update react + fluent

--- packages/webapp/src/index.tsx
+++ packages/webapp/src/index.tsx
@@ -2,7 +2,7 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import { initializeIcons } from '@fluentui/react/lib/Icons'
+import { initializeIcons } from '@fluentui/font-icons-mdl2'
 import { createRoot } from 'react-dom/client'
 
 import { App } from './App'
